3451ac350b20d3ffe190921f9cf6f7bd1e524e33
Arnav Doshi <127646166+Arknov@users.noreply.github.com>
2025-01-17 20:38:44 -0800

    Add files via upload

---
+++ src/main/java/com/appls/playerevolutions/util/RegistryHandler.java
@@ -0,0 +1,30 @@
+package com.appls.playerevolutions.util;
+
+import com.appls.playerevolutions.PlayerEvolutions;
+import com.appls.playerevolutions.blocks.BlockItemBase;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import com.appls.playerevolutions.items.ItemBase;
+import com.appls.playerevolutions.blocks.RubyBlock;
+
+public class RegistryHandler {
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, PlayerEvolutions.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, PlayerEvolutions.MOD_ID);
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+
+    }
+
+    private static Object ItemBase;
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+    private static Object RubyBlock;
+    public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
+    public static final RegistryObject<Item> RUBY_BLOCK_ITEM = ITEMS.register("ruby_block", () -> new BlockItemBase(RUBY_BLOCK.get()));
+
+
+}
